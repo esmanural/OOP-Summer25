ee74dff00079383cfb2af668d9daa09234b82329
esmanural <esmanur.ural@akademiata.edu.pl>
2025-03-06 11:01:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ class Person:
     def myfunc(self):
         print(f"Hello my name is {self.name}, Im {self.age} years old") Im living {self.address}
 # Example usage
-p1 = Person("Esmanur", 20, Poland-Warsaw )
+p1 = Person("Esmanur", 20, "Poland-Warsaw" )
 p1.myfunc()
