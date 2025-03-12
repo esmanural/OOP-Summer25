e5f920aec590e62fa6dad1a203849be4a6e1ae9b
esmanural <esmanur.ural@akademiata.edu.pl>
2025-03-12 19:12:43 +0100

    basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+my_height = 167  # int
+temperature = 39.9  # float
+greeting = "Hi Sir"  # str
+favorite_numbers = [7, 14, 21]  # list
+user_info = {"username": "Esmanur", "age": 20}  # dict
+coordinates = (40.7100, -74.0069)  # tup
+unique_letters = {"a", "b", "c"}  # set
+is_active = False  # bool
