eeb247de8dd15e0c26c2db9b9c8f8d56bafb1f23
esmanural <esmanur.ural@akademiata.edu.pl>
2025-03-13 10:37:03 +0100

    student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+student1 = {
+'first_name' = 'Esmanur',
+'last_name' = 'Ural',
+'index_number' = '34778',
+'nationality' = 'Turkey',
+'starting_date' = '2024-11-01',
+'courses' = ['Computer security', 'Match','Ethics']
+}
+print(student1.first_name)
+print(student1.last_name)
+print(student1.index_number)
+print(student1.nationality)
+print(student1.starting_date)
+print(student1.courses)
