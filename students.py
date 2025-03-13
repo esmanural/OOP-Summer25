69a496ae26e74795de716afcb01135941929befd
esmanural <esmanur.ural@akademiata.edu.pl>
2025-03-13 10:47:12 +0100

    students.py

---
+++ students.py
@@ -0,0 +1,32 @@
+students = [{
+ 'first_name' = 'Esmanur',
+'last_name' = 'Ural',
+'index_number' = '34572',
+'nationality' = 'Turkey',
+'starting_date' = '2024-11-01',
+'courses' = ['Computer security', 'Logistics','Ethics']
+},
+{
+ 'first_name' = 'Can',
+'last_name' = 'Aslan',
+'index_number' = 'TR342',
+'nationality' = 'Turkey',
+'starting_date' = '2024-09-01',
+'courses' = ['Computer Science', 'Match','Ethics']
+},
+{
+ 'first_name' = 'Baris',
+'last_name' = 'Mirza',
+'index_number' = 'TR32551',
+'nationality' = 'Turkey',
+'starting_date' = '2024-01-01',
+'courses' = ['English', 'Match','Marketing']
+}
+]
+for student in students:
+print(f"Student Name:
+{student['first_name']}
+{student['last_name']}
+{student['index_number']}
+)
+  
