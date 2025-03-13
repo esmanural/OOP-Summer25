2b46945fb2dcb5bf8909990a8b1c0f78528ba9e3
esmanural <esmanur.ural@akademiata.edu.pl>
2025-03-13 10:27:52 +0100

    student_details.py

--- student_details.py
+++ student_details.py
@@ -1,1 +1,6 @@
-student_details.py 
+first_name = "Esmanur"
+last_name = "Ural"
+index_number = "34778"
+nationality = "Turkey"
+starting_date = "2024-11-01 
+courses = ["Computer security", "Match","Ethics"]
